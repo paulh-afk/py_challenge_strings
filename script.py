52dc2cd71fef43773a8e26b25a646d425f1242ba
PauL <57185255+paulh-afk@users.noreply.github.com>
2022-11-23 19:39:37 +0100

    Count X function

--- script.py
+++ script.py
@@ -1,6 +1,8 @@
+# 1 - Count Letters
 letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
 
 
+# exercise | + fast for little word
 def unique_english_letters(word):
     unique_chars = str()
     for letter in word:
@@ -10,7 +12,36 @@ def unique_english_letters(word):
     return len(unique_chars)
 
 
-print("mississippi", unique_english_letters("mississippi"))
+# solution | + scalability
+def unique_english_letters_solution(word):
+    uniques = 0
+    for letter in letters:
+        if letter in word:
+            uniques += 1
+
+    return uniques
+
+
+print("mississippi", unique_english_letters_solution("mississippi"))
 # should print 4
-print("Apple", unique_english_letters("Apple"))
+print("Apple", unique_english_letters_solution("Apple"))
 # should print 4
+
+# 2 - Count X
+
+
+# exercise / same solution algorithm
+def count_char_x(word, x):
+    # return word.count(x)
+    count = 0
+    for letter in word:
+        if letter == x:
+            count += 1
+
+    return count
+
+
+print(count_char_x("mississippi", "s"))
+# should print 4
+print(count_char_x("mississippi", "m"))
+# should print 1
